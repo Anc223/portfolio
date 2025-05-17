7119e03b638b538e168ea36889b7353bce0c9b1e
ANCIYA A <Anc223>
2025-05-17 11:30:15 +0530

    main

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import Contactus from './Components/Contactus';
+import '@fortawesome/fontawesome-free/css/all.min.css';
+import "bootstrap/dist/js/bootstrap.bundle.min.js";
+import "bootstrap/dist/css/bootstrap.min.css";
+import Contactus2 from './Components/Contactus2';
+import Footer1 from './Components/Footer1';
+import Footer2 from './Components/Footer2';
+import Footer3 from './Components/Footer3';
+import Aboutus1 from './Components/Aboutus1';
+import Aboutus2 from './Components/Aboutus2';
+import Aboutus3 from './Components/Aboutus3';
+import Aboutus4 from './Components/Aboutus4';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <BrowserRouter>
+      <Routes>
+      <Route path='/Contactus' element={[<Contactus/>,<Contactus2/>,<Footer1/>,<Footer2/>,<Footer3/>]} />
+      <Route path='/Aboutus' element={[<Aboutus1/>,<Aboutus2/>,<Aboutus3/>,<Aboutus4/>,<Footer1/>,<Footer2/>,<Footer3/>]}/>
+
+      </Routes>
+      </BrowserRouter>
     </div>
   );
 }
